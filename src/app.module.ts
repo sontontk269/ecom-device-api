8b30299a3a59cf402952798cbd95e3ca1c779722
son <sonnguyentk269@gmail.com>
2025-08-15 22:06:49 +0700

    setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ConfigModule } from './config/config.module'
+import { PrismaModule } from './prisma/prisma.module'
+import { RedisModule } from './redis/redis.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [ConfigModule, PrismaModule, RedisModule],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
