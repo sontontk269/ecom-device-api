8b4aa2cae04f2f8d92e3c645a056b5215f1afb37
son <sonnguyentk269@gmail.com>
2025-08-25 01:44:46 +0700

    create default admin account

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common'
 import { CommonModule } from '@common/common.module'
 import { AppController } from 'src/app.controller'
 import { LoggerModule } from '@common/logger/logger.module'
+import { AdminInitService } from '@common/admin-init.service'
 import { UserModule } from '@modules/user/user.module'
 import { ConfigModule } from './config/config.module'
 import { PrismaModule } from './prisma/prisma.module'
 import { RedisModule } from './redis/redis.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { EmailModule } from './modules/email/email.module'
-import { ActivationModule } from './modules/activation/activation.module';
+import { ActivationModule } from './modules/activation/activation.module'
 
 @Module({
   imports: [
@@ -23,6 +24,6 @@ import { ActivationModule } from './modules/activation/activation.module';
     ActivationModule
   ],
   controllers: [AppController],
-  providers: []
+  providers: [AdminInitService]
 })
 export class AppModule {}
